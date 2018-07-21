2cf63d2c8245713031b61354060f602136c99ca6
projetCsharp <projetwebPISE@gmail.com>
2018-07-21 12:59:31 +0200

    ajout 1ere forme

--- CitadellesV1/Form1.Designer.cs
+++ CitadellesV1/Form1.Designer.cs
@@ -46,7 +46,6 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.reglesdujeu = new System.Windows.Forms.LinkLabel();
-            this.button1 = new System.Windows.Forms.Button();
             this.carteJ2.SuspendLayout();
             this.tabControl2.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -220,24 +219,11 @@
             this.reglesdujeu.Text = "Règles du jeu";
             this.reglesdujeu.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.reglesdujeu_LinkClicked);
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(408, 58);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 59);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Commencer une partie";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(948, 541);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.reglesdujeu);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label6);
@@ -282,7 +268,6 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.LinkLabel reglesdujeu;
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- CitadellesV1/Form1.cs
+++ CitadellesV1/Form1.cs
@@ -12,21 +12,72 @@ using System.Windows.Forms;
 
 namespace CitadellesV1
 {
+
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+            
         }
-
+        String nom1, nom2;
         private void Form1_Load(object sender, EventArgs e)
         {
+            var Commencer_partie = new Form();
+            Commencer_partie.StartPosition = FormStartPosition.CenterScreen;
+            
+
+            Label joueur1nom = new Label();
+            Label joueur2nom = new Label();
+            Button validationbouton = new Button();
+            validationbouton.Click += Validation_joueur;
+            TextBox nomjoueur1 = new TextBox();
+            TextBox nomjoueur2 = new TextBox();
+            Commencer_partie.Controls.Add(joueur1nom);
+            Commencer_partie.Controls.Add(joueur2nom);
+            Commencer_partie.Controls.Add(validationbouton);
+            Commencer_partie.Controls.Add(nomjoueur1);
+            Commencer_partie.Controls.Add(nomjoueur2);
+            joueur1nom.Text = "Veuillez entrer le nom du 1er joueur : ";
+            joueur2nom.Text = "Veuillez entrer le nom du 2nd joueur : ";
+            validationbouton.Text = "Commencer la partie";
+            joueur1nom.Left = 30;
+            joueur1nom.Width = 300;
+            nomjoueur1.Left = 30;
+            nomjoueur1.Top = 30;
+            nomjoueur1.Width = 100;
+            nomjoueur1.Height = 30;
+            joueur2nom.Left = 30;
+            joueur2nom.Top = 50;
+            joueur2nom.Width = 300;
+            nomjoueur2.Left = 30;
+            nomjoueur2.Top = 80;
+            nomjoueur2.Width = 100;
+            nomjoueur2.Height = 30;
+            validationbouton.Left = 50;
+            validationbouton.Top = 200;
+            validationbouton.Width = 200;
+            
+            nom1 = nomjoueur1.Text;
+            nom2 = nomjoueur2.Text;
+            Commencer_partie.ShowDialog(this);
+
             tabPage1.Text = "Personnages";
             tabPage3.Text = "Personnages";
             tabPage2.Text = "Quartiers";
             tabPage4.Text = "Quartiers";
 
         }
+
+        private void Validation_joueur(object sender, EventArgs e)
+        {
+            //string essai = nom1;
+            MessageBox.Show("les noms sont : "+nom1+" et "+nom2);
+        }
+
+        
 
         private void reglesdujeu_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
@@ -35,12 +86,5 @@ namespace CitadellesV1
             MessageBox.Show(regles,titre, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var Commencer_partie = new Form();
-            Commencer_partie.Show();
-            Button essaiebouton = new Button();
-            //Commencer_partie.essaiebouton.Text = "appuie ici et on verra";
-        }
     }
 }
